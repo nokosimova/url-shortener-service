c77d33c52aba32ddd74057553879efe179935687
nodirakosimova <nkosimova06@gmail.com>
2022-02-13 20:31:33 +0500

    add models

---
+++ Models/DbSettingModel.cs
@@ -0,0 +1,7 @@
+namespace LinkShortener.Models{
+    public class DbSettingModel{
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }         
+        public string CollectionName { get; set; }
+    }
+}
---
+++ Models/Link.cs
@@ -0,0 +1,19 @@
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LinkShortener.Models
+{
+    public class LinkItem
+    { 
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string LinkName  { get; set; } = null!;
+
+        public string ShortName  { get; set; } = null!;
+        public string CookieValue { get; set; } = null!;
+        public long VisitsCount { get; set; }
+    }
+}
